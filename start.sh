fa0b39b13e7a87f36c8e4ee1ac778222fb215588
SethDombrowski65 <SethDombrowski65@outlook.com>
2025-09-15 10:15:42 +0000

    feat: 更新启动脚本以优化服务启动流程

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ nohup ./opt/cloudreve -c config.ini > ./log/cd.log 2>&1 &
 nohup ./opt/cloudflared tunnel --url http://localhost:5212 > ./log/ccf.log 2>&1 &
 nohup ./opt/aria2c --conf-path=./aria2.conf > ./log/a2.log 2>&1 &
 nohup ./opt/komari -e https://tz.fsytool.top -t GAiw8-eIDTPme5G6 > ./log/km.log 2>&1 &
-nohup ./miniserve --port 8000 --upload ../downloads > ./log/ms.log 2>&1 &
+nohup ./opt/miniserve --port 8000 --upload ../downloads > ./log/ms.log 2>&1 &
 nohup ./opt/cloudflared tunnel --url http://localhost:8000 > ./log/mcf.log 2>&1 &
