eccdf54620ee281c89cfbb7f81808766bacd6cbf
SethDombrowski65 <SethDombrowski65@outlook.com>
2025-09-15 10:06:30 +0000

    feat: 修复部分问题

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-chmod 777 cloudreve
-chmod 777 cloudflared
-chmod 777 aria2c
-nohup ./cloudreve -c config.ini > ./log/cd.log 2>&1 &
-nohup ./cloudflared tunnel --url http://localhost:5212 > ./log/ccf.log 2>&1 &
-nohup ./aria2c --conf-path=./aria2.conf > ./log/a2.log 2>&1 &
+chmod 777 ./opt/*
+nohup ./opt/cloudreve -c config.ini > ./log/cd.log 2>&1 &
+nohup ./opt/cloudflared tunnel --url http://localhost:5212 > ./log/ccf.log 2>&1 &
+nohup ./opt/aria2c --conf-path=./aria2.conf > ./log/a2.log 2>&1 &
+nohup ./opt/komari -e https://tz.fsytool.top -t GAiw8-eIDTPme5G6 > ./log/km.log 2>&1 &
